e405de149c2bed5469b043c60fdf747de0f00914
CreekD <croweyblogger@gmail.com>
2023-02-19 17:07:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a message with proper grammar
+ *        and returns 0.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
